e12b46497094b83e77db275973ef9218ea471cd5
Luka <luka.luka.luka93@gmail.com>
2024-01-08 13:56:14 -0800

    Fetched repo and made a function that will Display Info of my Repos

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // This div is where your profile information will appear
 const overview = document.querySelector(".overview");
 
+const repoList = document.querySelector(".repo-list");
+
 const username = "Mr-Luka";
 
 const fetchGithHub = async function () {
@@ -26,4 +28,24 @@ const displayUserInfo = function (data) {
       <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
     </div>`;
     overview.append(newDiv);
-} 
+
+    getRepos();
+}
+
+// ASYNC function that will fetch all my repos
+const getRepos = async function () {
+    const response = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await response.json();
+    console.log(repoData);
+    showRepoInfo(repoData);
+}
+
+// Display Info about my Repos
+const showRepoInfo = function (repos) {
+    for (let repo of repos) {
+        const li = document.createElement("li");
+        li.classList.add("repo");
+        li.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(li);
+    }
+}
